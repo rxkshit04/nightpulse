9cf9f89d0b8a254b382fcd705b236865f8515c07
Rakshit Chauhan <email>
2025-04-17 15:07:27 +0530

    final changes

--- server/src/index.js
+++ server/src/index.js
@@ -12,7 +12,7 @@ app.get("/", (req, res) => {
 
 app.use("/api", geocodeRoutes);
 
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-module.exports = app;
+// module.exports = app;
